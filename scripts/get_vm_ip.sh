2edcb97f66d5047da069865acd738a53dc91cedd
Will Bui <wnbui@Home-Base.lan>
2026-03-17 23:43:14 -0700

    Add playbook for generating credentials

---
+++ scripts/get_vm_ip.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+VM_ID=$1
+PROXMOX_HOST="192.168.1.68"
+
+ssh -i ~/.ssh/homelab_ansible root@${PROXMOX_HOST} \
+  "qm guest cmd ${VM_ID} network-get-interfaces" | \
+  jq -r '.[1]."ip-addresses"[] | select(."ip-address-type" == "ipv4") | ."ip-address"' 2>/dev/null || echo "unknown"
